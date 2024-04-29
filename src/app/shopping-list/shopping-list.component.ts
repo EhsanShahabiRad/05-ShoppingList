2553e0627ba6af27c9cbc6ea539634105dc2ad8d
Ehsan Shahabirad <117647710+EhsanShahabiRad@users.noreply.github.com>
2024-04-29 14:24:57 +0200

    shopping-list and shopping-edit
    Implementing shopping-list and shopping-edit component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingrediant } from '../Shared/ingrediant.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.css'
+  styleUrl: './shopping-list.component.css',
 })
-export class ShoppingListComponent {
-
+export class ShoppingListComponent implements OnInit {
+  ingrediants: Ingrediant[] = [
+    new Ingrediant('Apple', 5),
+    new Ingrediant('Tomato', 10),
+  ];
+  constructor() {}
+  ngOnInit(): void {}
 }
